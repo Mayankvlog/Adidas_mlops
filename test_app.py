42624eb52798fa2e827b6ef2255929943d70ae6c
mayank singh <99034548+Mayankvlog@users.noreply.github.com>
2024-01-20 12:14:56 +0530

    Add files via upload

---
+++ test_app.py
@@ -0,0 +1,11 @@
+# test_app.py
+#import streamlit_app  # Assuming your main app file is named streamlit_app.py
+import adidas
+
+def test_user_input_features():
+    # Write test cases for the user_input_features function
+    pass
+
+def test_classifier_prediction():
+    # Write test cases for the classifier prediction function
+    pass
